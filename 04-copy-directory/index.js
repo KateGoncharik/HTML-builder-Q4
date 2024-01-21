0b7d89e22f8f30545c5060c1f6511fff811042b4
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-01-21 17:48:05 +0300

    feat: complete  the forth task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+const directoryToCopy = path.join(__dirname, 'files')
+const directoryToFill = path.join(__dirname, 'files-copy')
+
+fs.promises.rm(directoryToFill, { recursive: true, force: true }).then(()=>{
+    stdout.write('cleared')
+    fs.promises.mkdir(directoryToFill, {recursive:true}).then(()=> {
+        stdout.write('Directory created successfully');
+        fs.readdir(directoryToCopy, (err,files)=>{
+            if(err){
+                stdout.write(err)
+                process.exit(1)
+            }
+            else{
+                files.forEach((file)=>{
+                    fs.copyFile(path.join(directoryToCopy,file), path.join(directoryToFill, file), ()=> {
+                        stdout.write('copied')
+                    })
+                })
+            }
+            })
+    }).catch((err)=>{
+        stdout.write(err)
+        process.exit(1)
+    });
+});
+
