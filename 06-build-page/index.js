7c8d9e005deedb3619eb6e8fc102060a0988f299
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-01-22 22:33:54 +0300

    fix: correct final task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,115 +5,87 @@ const { stdout } = require('process');
 const stylesDirectory = path.join(__dirname, 'styles');
 const projectDist = path.join(__dirname, 'project-dist');
 
-const assetsDirectoryToCopy = path.join(__dirname, 'assets');
-const assetsDirectoryToFill = path.join(projectDist, 'assets');
+const assetsFrom = path.join(__dirname, 'assets');
+const assetsTo = path.join(projectDist, 'assets');
+
+function handleError(err) {
+  if (err) {
+    stdout.write(`${err}`);
+    process.exit(1);
+  }
+}
 
 function copyAllNestedFiles(sourcePath, destinationPath) {
   fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
-    if (err) {
-      stdout.write(`${err}`);
-      process.exit(1);
-    }
+    handleError(err);
     files.forEach((file) => {
       const sourceFilePath = path.join(sourcePath, file.name);
       const destinationFilePath = path.join(destinationPath, file.name);
       if (file.isDirectory()) {
-        fs.promises.mkdir(path.join(destinationFilePath)).then(() => {
+        fs.promises.mkdir(destinationFilePath).then(() => {
           copyAllNestedFiles(sourceFilePath, destinationFilePath);
         });
       } else {
-        fs.copyFile(sourceFilePath, destinationFilePath, (err) => {
-          if (err) stdout.write(`${err}`);
-        });
+        fs.copyFile(sourceFilePath, destinationFilePath, handleError);
       }
     });
   });
 }
 
 function bundleStyles() {
-  fs.writeFile(projectDist, 'style.css', () => {
-    stdout.write('Styles bundled successfully\n');
-  });
   fs.readdir(stylesDirectory, (err, files) => {
-    if (err) {
-      stdout.write(err);
-      process.exit(1);
-    } else {
-      files.forEach((file) => {
-        const stream = fs.createReadStream(
-          path.join(stylesDirectory, file),
-          'utf-8',
-        );
-        stream.on('data', (data) =>
-          fs.appendFile(path.join(projectDist, 'style.css'), data, (err) => {
-            if (err) {
-              stdout.write(err);
-              process.exit(1);
-            }
-          }),
-        );
-        stream.on('error', (err) => stdout.write(`${err}`));
-      });
-    }
+    handleError(err);
+    files.forEach((file) => {
+      const stream = fs.createReadStream(
+        path.join(stylesDirectory, file),
+        'utf-8',
+      );
+      stream.on('data', (data) =>
+        fs.appendFile(path.join(projectDist, 'style.css'), data, handleError),
+      );
+      stream.on('error', handleError);
+    });
   });
 }
 
 function createIndexHtml() {
-  fs.appendFile(path.join(projectDist, 'index.html'), '', () => {
-    stdout.write('Index.html created successfully\n');
-  });
-  fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
-    if (err) {
-      stdout.write(err);
-      process.exit(1);
-    } else {
-      files.forEach((file) => {
-        if (file.name !== 'template.html') {
-          return;
+  const readableStreamTemplate = fs.createReadStream(
+    path.join(__dirname, 'template.html'),
+    'utf-8',
+  );
+  readableStreamTemplate.on('data', (chunk) => {
+    let template = chunk;
+
+    const regex = /\{\{([^}]+)\}\}/g;
+    const tags = template.match(regex);
+
+    tags.forEach((tag) => {
+      const componentName = `${tag.slice(2, -2)}.html`;
+
+      fs.readdir(path.join(__dirname, 'components'), (err, files) => {
+        if (!files.includes(componentName)) {
+          console.log(`${componentName} is not in ${files}`);
+          process.exit(1);
         }
-        const readableStream = fs.createReadStream(
-          path.join(__dirname, file.name),
-          'utf-8',
-        );
-        readableStream.on('data', (chunk) => {
-          let template = chunk;
-          const readableStreamFooter = fs.createReadStream(
-            path.join(__dirname, 'components', 'footer.html'),
-            'utf-8',
-          );
-          readableStreamFooter.on('data', (chunk) => {
-            template = template.replace('{{footer}}', chunk);
-          });
-          const readableStreamHeader = fs.createReadStream(
-            path.join(__dirname, 'components', 'header.html'),
-            'utf-8',
-          );
-          readableStreamHeader.on('data', (chunk) => {
-            template = template.replace('{{header}}', chunk);
-          });
-          const readableStreamArticles = fs.createReadStream(
-            path.join(__dirname, 'components', 'articles.html'),
-            'utf-8',
-          );
-          readableStreamArticles.on('data', (chunk) => {
-            template = template.replace('{{articles}}', chunk);
-            fs.writeFile(
-              path.join(projectDist, 'index.html'),
-              template,
-              () => {},
-            );
-          });
-        });
       });
-    }
+
+      const componentContentStream = fs.createReadStream(
+        path.join(__dirname, 'components', componentName),
+        'utf-8',
+      );
+      componentContentStream.on('data', (chunk) => {
+        template = template.replace(tag, chunk);
+        fs.writeFile(path.join(projectDist, 'index.html'), template, () => {});
+      });
+    });
   });
 }
 
 fs.promises.rm(projectDist, { recursive: true, force: true }).then(() => {
   fs.promises.mkdir(projectDist).then(() => {
-    fs.promises.mkdir(assetsDirectoryToFill);
+    fs.promises.mkdir(assetsTo);
     stdout.write('Directory created successfully\n');
-    copyAllNestedFiles(assetsDirectoryToCopy, assetsDirectoryToFill);
+    copyAllNestedFiles(assetsFrom, assetsTo);
     bundleStyles();
     createIndexHtml();
   });
